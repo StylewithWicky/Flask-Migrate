986e7d0d3af779a37c89e7fc3bfd2af1e8549048
Yazz <itsjustmaitai@gmail.com>
2025-06-18 11:57:10 +0300

    Created Author Model:

---
+++ models/author.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db=SQLAlchemy()
+
+class Author(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(100),nullable= False)
+
+    books=db.relationship('Book', backref='author')
+
+
